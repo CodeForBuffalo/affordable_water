81b50a51e77fa7d456fa8698422e43e777e9eafc
Michael Brown <mbrown@buffalosewer.org>
2019-09-06 11:08:14 -0400

    Merged Address model into Account model

--- pathways/models.py
+++ pathways/models.py
@@ -5,13 +5,12 @@ from PIL import Image
 # Create your models here.
 class Application(models.Model):
     first_name = models.CharField(max_length=100)
-    middle_initial = models.CharField(blank=True, default='')
+    middle_initial = models.CharField(blank=True, default='', max_length=5)
     last_name = models.CharField(max_length=100)
 
-    # https://www.codeproject.com/Questions/756671/convert-number-to-phone-number-format-using-java-s
-    # https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    phone_number = models.CharField(validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")],
+        max_length=17, blank=True) # validators should be a list
 
     email_address = models.EmailField(blank=True)
 
@@ -22,21 +21,21 @@ class Documents(models.Model):
         ('TWK',"Every two weeks"),
         ('TMO','Twice a month'),
         ('MO','Every month')],
-        )
+        max_length=100)
     income = models.IntegerField()
-    # pay stub or pay checks, any proof of other income
     residency_photo = models.ImageField(upload_to='residency_docs')
     income_photo = models.ImageField(upload_to='income_docs')
 
 class Account(models.Model):
     application = models.OneToOneField(Application, on_delete=models.CASCADE)
+    # Account holder
     account_first = models.CharField(max_length=100)
-    account_middle = models.CharField(blank=True, default='')
+    account_middle = models.CharField(blank=True, default='', max_length=5)
     account_last = models.CharField(max_length=100)
 
-class Address(models.Model):
-    application = models.OneToOneField(Application, on_delete=models.CASCADE)
+    # Account service address
     address_number = models.IntegerField()
-    address_street = models.CharField()
-    address_apartment = models.CharField()
+    address_street = models.CharField(max_length=100)
+    address_apartmentnum = models.CharField(max_length=100)
     address_zip = models.IntegerField()
+    
