a877c471c35957e9eb879a2ae8edf3e9cab921ba
Michael Brown <mbrown@buffalosewer.org>
2019-09-09 13:53:43 -0400

    Modified apply view, uses session data

--- pathways/urls.py
+++ pathways/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='pathways-home'),
     path('about/', views.about, name='pathways-about'),
-    path('apply/', views.apply, name='apply'),
+    path('apply/', views.apply, name='pathways-apply'),
 ]
--- pathways/views.py
+++ pathways/views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from .forms import ApplicationForm, DocumentUploadForm, AccountAddressForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'pathways/home.html')
+    context = {}
+    if request.session.has_key('app_id'):
+        context['app_id'] = request.session['app_id']
+    return render(request, 'pathways/home.html', context)
 
 def about(request):
     return render(request, 'pathways/about.html', {'title':'About'})
 
 def apply(request):
+    context = {}
+    initial = {'app_id': request.session.get('app_id', None)}
+    form = ApplicationForm(request.POST or None, initial=initial)
     if request.method == 'POST':
-        form = ApplicationForm(request.POST)
         if form.is_valid():
             # form.save()
-            app_id = form.cleaned_data.get('id')
+            app_id = form.clean_phone_number()
             request.session['app_id'] = app_id
-            return render(request, 'pathways/home.html')
+            messages.info(request, f'Test submit!')
+            return redirect('/')
     else:
         form = ApplicationForm()
-    return render(request, 'pathways/apply.html', {'form':form})
+    context['form'] = form
+    return render(request, 'pathways/apply.html', context)
