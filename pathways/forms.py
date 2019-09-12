66a8525dc716417232e9516ee1a62d6b331036af
Michael Brown <mbrown@buffalosewer.org>
2019-09-12 13:25:05 -0400

    Modified AccountForm
    - If user marks "Yes" they are the same person as who is on the account, then upon submission the form will automatically use their name for the account name
    
    - If user selects "No", they will be required to fill in the contact address of whoever is on the account

--- pathways/forms.py
+++ pathways/forms.py
@@ -28,13 +28,45 @@ class DocumentForm(ModelForm):
         fields = ['pay_period','income','residency_photo','income_photo']
 
 class AccountForm(ModelForm):
-    isAccountNameSame = forms.ChoiceField(required=True, initial=False,
-        label=_("Is your name listed on the water bill?"), choices=(('Yes',"Yes"),('No',"No")), widget=widgets.Select,
+    isAccountNameSame = forms.ChoiceField(required=True, initial=None, 
+        label=_("Is your name listed on the water bill?"), 
+        choices=(('-',"-"),(True,"Yes"),(False,"No")),
+        widget=widgets.Select(attrs= {'onchange': ""}), # TODO add onchange function to hide account
         help_text=_("The person whose name is on the water bill. This might be a partner or roommate."))
+
+    account_first = forms.CharField(max_length=100, required=False)
+    account_middle = forms.CharField(max_length=5, required=False)
+    account_last = forms.CharField(max_length=100, required=False)
 
     class Meta:
         model = Account
         fields = ['isAccountNameSame','account_first','account_middle','account_last',
             'address_number','address_street','address_apartment_number','address_zip']
+    
+    # https://www.fusionbox.com/blog/detail/creating-conditionally-required-fields-in-django-forms/577/
+    def fields_required(self, fields):
+        """Used for conditionally marking fields as required."""
+        for field in fields:
+            if not self.cleaned_data.get(field, ''):
+                msg = forms.ValidationError("This field is required.")
+                self.add_error(field, msg)
+
+    def clean(self):
+        if self.cleaned_data.get('isAccountNameSame') == 'True':
+            for field in ['account_first','account_middle','account_last']:
+                self.fields[field].required = False
+            app = Application.objects.filter(id=self.app_id)[0]
+            self.cleaned_data['account_first'] = app.first_name
+            self.cleaned_data['account_middle'] = app.middle_initial
+            self.cleaned_data['account_last'] = app.last_name
+        else:
+            self.fields_required(['account_first','account_middle','account_last'])
+        
+        return self.cleaned_data
+
+    # https://stackoverflow.com/questions/18605008/curious-about-get-form-kwargs-in-formview
+    def __init__(self, *args, **kwargs):
+        self.app_id = kwargs.pop('app_id', None)
+        super(AccountForm, self).__init__(*args, **kwargs)
 
     
