00ba544f89511e6275af260be5c13b3dae64f2bf
Mike Brown <mpbrown@buffalo.edu>
2019-10-28 13:31:53 -0400

    Added selenium, coverage, and pytest-cov

--- pathways/tests.py
+++ pathways/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Create your tests here.
+
+class TestExample(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_foo(self):
+        self.assertEquals(0,0)
+
+    def test_python_page(self):
+        self.browser.get('http://www.python.org')
+        assert "Python" in self.browser.title
+        elem = self.browser.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results founds" not in self.browser.page_source
+
