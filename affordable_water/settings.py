b8135145b30a03d0a51ad008aa020a2e31fda208
Mike Brown <mpbrown@buffalo.edu>
2019-10-28 18:05:44 -0400

    Update settings.py

--- affordable_water/settings.py
+++ affordable_water/settings.py
@@ -35,6 +35,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 # This expression evaluates to false if DEBUG_VALUE is not set as environment variable
 DEBUG = (os.getenv('DEBUG_VALUE') == 'True')
+DEBUG_PROPAGRATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -194,3 +195,36 @@ CSRF_COOKIE_SECURE = (os.getenv('PRODUCTION_VALUE') == 'True')
 SESSION_COOKIE_SECURE = (os.getenv('PRODUCTION_VALUE') == 'True')
 
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
