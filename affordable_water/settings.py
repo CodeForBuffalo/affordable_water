545134e2c3d4a405185d6a0b5c4d3beb90630003
Mike Brown <mpbrown@buffalo.edu>
2019-10-30 08:11:24 -0400

    Update settings.py

--- affordable_water/settings.py
+++ affordable_water/settings.py
@@ -167,7 +167,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pathways/static'),
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, '_vendor'),
 ]
 
