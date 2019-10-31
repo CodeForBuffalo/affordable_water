b65887fe47ae4962d1897d65e18f4d9a306d0a28
Mike Brown <mpbrown@buffalo.edu>
2019-10-31 12:43:07 -0400

    Removed old code for users app
    - Removed registration
    - Removed Profile
    - Removed crispy-forms

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -17,8 +16,3 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['image']
