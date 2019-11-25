4bc6cf5bd18aa6b00f66d868f5f7a6047b3687b5
Mike Brown <mpbrown@buffalo.edu>
2019-11-25 10:16:05 -0500

    Initialize selenium tests
    - Update .travis.yml

---
+++ pathways/tests/test_selenium.py
@@ -0,0 +1,29 @@
+from django.test import LiveServerTestCase
+from django.urls import reverse
+from django.utils.translation import activate
+from django.utils.translation import ugettext_lazy as _
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+if (os.getenv('TRAVIS_CHROME_PATH') == None):
+    chrome_driver = "node_modules/chromedriver/lib/chromedriver/chromedriver.exe"
+else:
+    chrome_driver = "/usr/local/share/chromedriver"
+
+class PathwaysTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+        activate('en')
+        super(PathwaysTestCase, self).setUp()
+
+    def tearDown(self):
+        self.driver.quit()
+        super(PathwaysTestCase, self).tearDown()
+
+    def test_home(self):
+        self.driver.get(f"{self.live_server_url}")
+        self.assertIn('GetBuffaloWater', self.driver.title)
+        
