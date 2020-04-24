132d284b44a67b5a0b7f54c003ccfb8cd155ac82
Mike Brown <mpbrown@buffalo.edu>
2020-04-24 08:44:02 -0400

    Update password reset flow to fit with two factor authentication

--- affordable_water/settings.py
+++ affordable_water/settings.py
@@ -201,11 +201,11 @@ MEDIA_URL = '/media/'
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_PORT = 587
 
 # AWS
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
