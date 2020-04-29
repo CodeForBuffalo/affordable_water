d62e0e6eb2a0c08588ebb317d0e78bde7db9e74b
Mike Brown <mpbrown@buffalo.edu>
2020-04-29 15:27:26 -0400

    Update email to custom domain

--- affordable_water/settings.py
+++ affordable_water/settings.py
@@ -202,7 +202,7 @@ MEDIA_URL = '/media/'
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.fastmail.com'
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 EMAIL_PORT = 587
