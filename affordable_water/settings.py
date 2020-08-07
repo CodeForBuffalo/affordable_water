c77056fcf176caa52276779e6e5ad63240c85a2a
Mike Brown <mpbrown@buffalo.edu>
2020-08-07 12:43:26 -0400

    Limit worker concurrency in Celery

--- affordable_water/settings.py
+++ affordable_water/settings.py
@@ -218,7 +218,6 @@ CELERY_BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux 
 CELERY_RESULT_BACKEND = None # AMQP is not recommended as result backend as it creates thousands of queues
 CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
 CELERY_WORKER_PREFETCH_MULTIPLIER = 1 # Disable prefetching, it's causes problems and doesn't help performance
-CELERY_WORKER_CONCURRENCY = 50 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
